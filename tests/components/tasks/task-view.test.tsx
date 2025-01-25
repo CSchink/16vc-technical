1677ab386fa19a3925d6fa7288d62f5ce9e175b0
Corey Schink <corey.schink@gmail.com>
2025-01-25 17:18:42 -0500

    moved test folders

--- tests/components/tasks/task-view.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import TaskView from "../../../src/components/tasks/task-view"
-
-test('renders button with correct label', () => {
-  const { getByText } = render(<TaskView />);
-  const buttonElement = getByText('Click me');
-  expect(buttonElement).toBeFalsy();
-});
