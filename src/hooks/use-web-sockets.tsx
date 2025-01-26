f9c28854515d3ac524143c52837bb1bd9e3f1bf5
Corey Schink <corey.schink@gmail.com>
2025-01-26 17:38:16 -0500

    update ws hooks

--- src/hooks/use-web-sockets.tsx
+++ src/hooks/use-web-sockets.tsx
@@ -4,16 +4,27 @@ import * as Ably from "ably";
 import { isEqual } from "lodash";
 import { CHANNELS } from "../api/schemas/ws.schemas";
 import iTools from "../api/utils/i-tools";
-import { useChannel } from "ably/react";
-
+import { useChannel, useConnectionStateListener } from "ably/react";
 
 export const useWS = () => {
   const [messages, setMessages] = useState<Ably.Message[]>([]);
+  const [readyState, setReadyState] = useState<
+    Ably.ConnectionStateChange | undefined
+  >(undefined);
   const { publish } = useChannel(CHANNELS.tasks, (message) => {
     setMessages((prev) => [...prev, message]);
   });
+
+  useConnectionStateListener((stateChange) => {
+    console.log(stateChange.current); // the new connection state
+    console.log(stateChange.previous); // the previous connection state
+    console.log(stateChange.reason); // if applicable, an error indicating the reason for the connection state change
+    console.log(readyState)
+    setReadyState(stateChange);
+  });
+
   const sendMessage = (messageText: any) => {
-    iTools.log(`Sending message: ${messageText}`)
+    iTools.log(`Sending message: ${messageText}`);
     publish(CHANNELS.tasks, { message: messageText });
   };
 
