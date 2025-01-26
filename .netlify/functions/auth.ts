0d16969988f826fd5d7aa54ad1d57ebc15ac62c0
Corey Schink <corey.schink@gmail.com>
2025-01-26 15:02:26 -0500

    update function config

--- .netlify/functions/auth.ts
+++ .netlify/functions/auth.ts
@@ -5,28 +5,34 @@ import { HandlerEvent, HandlerContext } from "@netlify/functions";
 dotenv.config();
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default async function handler(event: HandlerEvent, _context: HandlerContext) {
-  if (!process.env.ABLY_API_KEY) {
+export default async function handler(
+  event: HandlerEvent,
+  _context: HandlerContext
+) {
+  try {
+    if (!process.env.ABLY_API_KEY) {
+      return {
+        statusCode: 500,
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(`Missing ABLY_API_KEY environment variable.`),
+      };
+    }
+    console.log(event);
+    const clientId =
+      event.queryStringParameters?.["clientId"] ||
+      process.env.DEFAULT_CLIENT_ID ||
+      "NO_CLIENT_ID";
+    const client = new Ably.Rest(process.env.ABLY_API_KEY);
+    const tokenRequestData = await client.auth.createTokenRequest({
+      clientId: clientId,
+    });
     return {
-      statusCode: 500,
+      statusCode: 200,
       headers: { "content-type": "application/json" },
-      body: JSON.stringify(
-        `Missing ABLY_API_KEY environment variable.`
-      ),
+      body: JSON.stringify(tokenRequestData),
     };
+  } catch (e) {
+    console.log(event);
+    return e.message
   }
-  console.log(event);
-  const clientId =
-    event.queryStringParameters?.["clientId"] ||
-    process.env.DEFAULT_CLIENT_ID ||
-    "NO_CLIENT_ID";
-  const client = new Ably.Rest(process.env.ABLY_API_KEY);
-  const tokenRequestData = await client.auth.createTokenRequest({
-    clientId: clientId,
-  });
-  return {
-    statusCode: 200,
-    headers: { "content-type": "application/json" },
-    body: JSON.stringify(tokenRequestData),
-  };
 }
