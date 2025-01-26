fccd24149c3f99d0f9ff8e1ee5ecd1ef10d619a3
Corey Schink <corey.schink@gmail.com>
2025-01-26 10:46:09 -0500

    add error boundary

---
+++ src/components/common/error-boundary.tsx
@@ -0,0 +1,34 @@
+import { Component, ReactNode } from "react";
+import { handleException } from "./utils/handle-error";
+
+interface Props {
+  children?: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  public state: State = {
+    hasError: false,
+  };
+
+  public static getDerivedStateFromError(_: Error): State {
+    return { hasError: true };
+  }
+
+  public componentDidCatch(error: unknown) {
+    handleException(error);
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      return <h1>Sorry.. there was an error</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
